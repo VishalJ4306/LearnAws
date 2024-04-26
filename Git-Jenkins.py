6391b0becf39319cb7e6050f485dd033ba27e2c2
Dev-Vishal <166522861+VishalJ4306@users.noreply.github.com>
2024-04-26 10:48:25 +0530

    Update3 Git-Jenkins.py

--- Git-Jenkins.py
+++ Git-Jenkins.py
@@ -1,3 +1,4 @@
 # Program for just showing string from python 
 print("This example for pull code from GitHub")
 print("this is change for testing of auto build in jenkins")
+print("this is for POLL SCM example for auto build")
