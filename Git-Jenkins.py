0907253790cb0e615c8f4ad287172cbc13f81a85
Dev-Vishal <166522861+VishalJ4306@users.noreply.github.com>
2024-04-26 11:48:44 +0530

    Update4 Git-Jenkins.py

--- Git-Jenkins.py
+++ Git-Jenkins.py
@@ -3,3 +3,4 @@ print("This example for pull code from GitHub")
 print("this is change for testing of auto build in jenkins")
 print("this is for POLL SCM example for auto build")
 print("this is auto build after one minute")
+print(
